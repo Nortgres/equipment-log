cae55f176287bb3213fb45a8b23d252c38e27d40
Nortgres <sergtron@ya.ru>
2024-02-24 19:45:47 +0300

    add files

--- equipment/eqlog/admin.py
+++ equipment/eqlog/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from models import Person, Equipment
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('id', 'last_name', 'first_name', 'middle_name', 'department', 'job_title', 'created_at', 'remote', 'city',
+                    'is_working')
+    list_display_links = ('id', 'last_name')
+    search_fields = ('last_name', 'first_name')
+    list_editable = ('is_working',)
+    list_filter = ('is_working', 'remote', 'city', 'created_at',)
+    prepopulated_fields = {"slug": ("last_name", )}
+
+@admin.register(Equipment)
+class EquipmentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'type', 'model', 'serial_number', 'id_number', 'sale_date', 'price', 'is_working', 'testing',
+                    'description')
+    list_display_links = ('id', 'model')
+    search_fields = ('type', 'id_number')
+    list_editable = ('is_working',)
+    list_filter = ('is_working', 'sale_date', 'type', 'id_number',)
+    prepopulated_fields = {"slug": ("last_name", )}
--- equipment/eqlog/models.py
+++ equipment/eqlog/models.py
@@ -54,3 +54,4 @@ class Person(models.Model):
 
     def __str__(self):
         return self.fio
+
