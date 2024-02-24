9feaf22a942e35a407dee2ed388b98ffc2a43441
Nortgres <sergtron@ya.ru>
2024-02-24 14:41:10 +0300

    add files

--- equipment/eqlog/views.py
+++ equipment/eqlog/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.contrib.auth import logout, login
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.views import LoginView
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
 
-def index(request)
-
+def about(request):
+    return render(request, 'eqlog/about.html', {'menu': menu, 'title': 'О сайте'})
